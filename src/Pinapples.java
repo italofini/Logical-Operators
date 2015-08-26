327739693c232626a37da61e9a3e82d2f7a90fcd
italofini <italo.fini@techhigh.us>
2015-08-25 23:58:09 -0400

    LO

---
+++ src/Pinapples.java
@@ -0,0 +1,15 @@
+
+public class Pinapples {
+	public static void main (String args[]){
+		int boy, girl;
+		boy = 8;
+		girl = 99;
+		
+		if (boy >10 || girl <99) {
+			System.out.println("You can enter");
+		}else{
+			System.out.println("You can not enter");
+		}
+	}
+
+}
